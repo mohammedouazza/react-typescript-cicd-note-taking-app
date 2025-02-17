3f8997b2f454cf4ff509ee01b8d1bafb84803286
mohammedouazza <ouazza.mohammed95@gmail.com>
2025-02-17 02:14:02 +0100

    setup react routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-typescript-cicd-note-taking-app/",
+  //base: "/react-typescript-cicd-note-taking-app/",
 });
